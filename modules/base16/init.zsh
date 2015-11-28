37452e6d32441b8947d9aa19f2510c6d29f6416d
David Mason <djmason@gmail.com>
2015-11-29 00:00:00 +1100

    Only source base16 256 colour remap if terminal supports 256 colors

--- modules/base16/init.zsh
+++ modules/base16/init.zsh
@@ -1,5 +1,7 @@
 zstyle -a ':prezto:module:base16' theme 'BASE16_THEME'
 
-source ${0:h}/external/base16-${BASE16_THEME[1]-default}.${BASE16_THEME[2]-dark}.sh
+if [[ $(tput colors) == "256" ]]; then
+    source ${0:h}/external/base16-${BASE16_THEME[1]-default}.${BASE16_THEME[2]-dark}.sh
+fi
 
 unset BASE16_THEME
